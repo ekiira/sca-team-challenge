088841f4dee967b556993700620b88f71f544f2f
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2021-04-25 20:01:23 +0100

    remove mobile navigation on nav-link click

--- script/index.js
+++ script/index.js
@@ -1,6 +1,7 @@
 const menuIcon = document.querySelector(".menu-icon");
 const menuButton = document.querySelector("#hamburger-button");
 const mobileNav = document.querySelector("#mobile-nav");
+const mobileNavLinks = document.querySelectorAll(".mobile-nav");
 
 menuButton.addEventListener("click", () => {
   if (mobileNav.classList.contains("d-none")) {
@@ -8,6 +9,16 @@ menuButton.addEventListener("click", () => {
     menuIcon.src = "./images/icon-close.svg";
   } else {
     mobileNav.className = mobileNav.className.replace("d-block", "d-none");
-    menuIcon.src = "./images/icon-hamburger.svg"
+    menuIcon.src = "./images/icon-hamburger.svg";
   }
+});
+
+const links = [...mobileNavLinks];
+
+links.map((link, i) => {
+  link.addEventListener("click", () => {
+    mobileNav.className = mobileNav.className.replace("d-block", "d-none");
+    mobileNav.className = mobileNav.className.replace("d-block", "d-none");
+    menuIcon.src = "./images/icon-hamburger.svg";
+  });
 });
